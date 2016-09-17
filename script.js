a624ab32402389defd40b5454cc691f866909835
abhinav <abhi.aec89@gmail.com>
2016-09-17 23:40:31 +0530

    CORS ERROR

--- script.js
+++ script.js
@@ -5,12 +5,26 @@
     "use strict";
     angular.module("myApp",[])
         .controller("myController",["$scope","$http",function ($scope,$http) {
-            const UBER_KEY = "ECWcv5urK26d-pz-OHio9c9ovHpahx4UBbQIzMTi";
-            const GOOGLE_KEY = "AIzaSyB6ky0s6kmaxH15hsxsNHKuZeI6n_OG2eA";
-            var uber_url = "";
-            var google_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=driving&language=fr-FR&key=" + GOOGLE_KEY;
-            $scope.callUber = function (time) {
+            $scope.logs = [];
+            var callGoogle = function () {
+                $http.get("/google",{
+                    'sourceLat':$scope.sourceLat,
+                    'sourceLong': $scope.sourceLong,
+                    'destLat': $scope.destLat,
+                    'destLong': $scope.destLong
+                }).then(function (data) {
+                    console.log(data);
+                    var temp = Date.now() + ": Google API Call for " + $scope.notifyEmail;
+                    $scope.logs.push(temp.toString());
+                    temp = null;
+                },function (error) {
+                    $scope.logs.push(error);
+                    console.log(error);
+                });
+            }
 
+            $scope.begin = function () {
+                callGoogle();
             }
 
         }])
@@ -35,21 +49,21 @@
  1) Poll Google Map (PG1) and get exact delay (DG1).
  2) Poll Uber (PU1) ang get delay(DU1);
  3) Assuming DG1 > DU1,
-    if DG1 + time without traffic + DU1 = now {
-        i) then book the cab and send email
-    } else {
-        i) D1 = DG0 - DG1;
-        ii) PG2 = D1 / 2; (Binary search: O(log n))
-        iii) Set timer(Time T2) of which will trigger at = now + PG2
-    }
+ if DG1 + time without traffic + DU1 = now {
+ i) then book the cab and send email
+ } else {
+ i) D1 = DG0 - DG1;
+ ii) PG2 = D1 / 2; (Binary search: O(log n))
+ iii) Set timer(Time T2) of which will trigger at = now + PG2
+ }
  4) At the Time T2, Poll Google Map (PG2) and get to know delay (DG2).
  5)  if (DG2 + time without traffic + DU1 - now <= 5 minutes) {
-        i) then book the cab
-        ii) Send email
-        iii) return and exit form program
-    } else {
-        i) Go to step 3 and continue binary search recursively
-    }
+ i) then book the cab
+ ii) Send email
+ iii) return and exit form program
+ } else {
+ i) Go to step 3 and continue binary search recursively
+ }
 
  So this problem can be solved at time complexity of O(Log n)
  Least number of polls:
