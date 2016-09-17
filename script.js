393cb327eaad11d748993b9072854bf6c7d5770b
abhinav <abhi.aec89@gmail.com>
2016-09-17 16:47:52 +0530

    Initial Changes

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by abhinav on 17/9/16.
+ */
+(function () {
+    "use strict";
+    angular.module("myApp",[])
+        .controller("myController",["$scope","$http",function ($scope,$http) {
+            
+        }])
+})();
